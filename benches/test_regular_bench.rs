eeeb3670caf0b74f61b055ed7e97a4487c847fe8
Joining7943 <joining@posteo.de>
2023-03-09 20:34:41 +0100

    bench: Fix test_regular_bench

--- benches/test_regular_bench.rs
+++ benches/test_regular_bench.rs
@@ -2,21 +2,22 @@ use iai_callgrind::{black_box, main};
 
 fn fibonacci(n: u64) -> u64 {
     match n {
-        0 => 1,
-        1 => 1,
+        0 | 1 => 1,
         n => fibonacci(n - 1) + fibonacci(n - 2),
     }
 }
 
-// Don't forget the `#[inline(never)]`
 #[inline(never)]
-fn iai_benchmark_short() -> u64 {
+fn bench_empty() {}
+
+#[inline(never)]
+fn bench_fibonacci() -> u64 {
     fibonacci(black_box(10))
 }
 
 #[inline(never)]
-fn iai_benchmark_long() -> u64 {
+fn bench_fibonacci_long() -> u64 {
     fibonacci(black_box(30))
 }
 
-main!(iai_benchmark_short, iai_benchmark_long);
+main!(bench_empty, bench_fibonacci, bench_fibonacci_long);
