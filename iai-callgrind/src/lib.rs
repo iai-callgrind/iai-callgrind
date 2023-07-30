c0e06985d63c7b584dda21638643f20dd6dee437
Joining7943 <joining@posteo.de>
2023-07-30 13:16:25 +0200

    iai-callgrind: Use stricter clippy rules and apply them

--- iai-callgrind/src/lib.rs
+++ iai-callgrind/src/lib.rs
@@ -1,7 +1,33 @@
-use std::ffi::OsString;
-use std::path::PathBuf;
+//! library of iai-callgrind with helper structs and methods for the main macro
+
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc(test(attr(warn(unused))))]
+#![doc(test(attr(allow(unused_extern_crates))))]
+#![warn(missing_docs)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::default_numeric_fallback)]
+#![warn(clippy::else_if_without_else)]
+#![warn(clippy::fn_to_numeric_cast_any)]
+#![warn(clippy::get_unwrap)]
+#![warn(clippy::if_then_some_else_none)]
+#![warn(clippy::mixed_read_write_in_expression)]
+#![warn(clippy::partial_pub_fields)]
+#![warn(clippy::rest_pat_in_fully_bound_structs)]
+#![warn(clippy::str_to_string)]
+#![warn(clippy::string_to_string)]
+#![warn(clippy::todo)]
+#![warn(clippy::try_err)]
+#![warn(clippy::undocumented_unsafe_blocks)]
+#![warn(clippy::unneeded_field_pattern)]
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::return_self_not_must_use)]
+#![allow(clippy::enum_glob_use)]
+#![allow(clippy::module_name_repetitions)]
 
 mod macros;
+
+use std::ffi::OsString;
+use std::path::PathBuf;
 
 /// A function that is opaque to the optimizer, used to prevent the compiler from
 /// optimizing away computations in a benchmark.
@@ -9,6 +35,7 @@ mod macros;
 /// This variant is stable-compatible, but it may cause some performance overhead
 /// or fail to prevent code from being eliminated.
 pub fn black_box<T>(dummy: T) -> T {
+    // SAFETY: The safety conditions for read_volatile and forget are satisfied
     unsafe {
         let ret = std::ptr::read_volatile(&dummy);
         std::mem::forget(dummy);
@@ -16,10 +43,14 @@ pub fn black_box<T>(dummy: T) -> T {
     }
 }
 
+/// TODO: DOCUMENT
 #[derive(Debug)]
 pub struct Options {
+    /// TODO: DOCUMENT
     pub env_clear: bool,
+    /// TODO: DOCUMENT
     pub current_dir: Option<PathBuf>,
+    /// TODO: DOCUMENT
     pub entry_point: Option<String>,
 }
 
@@ -30,6 +61,7 @@ impl Default for Options {
 }
 
 impl Options {
+    /// TODO: DOCUMENT
     pub fn new() -> Self {
         Self {
             env_clear: true,
@@ -38,32 +70,39 @@ impl Options {
         }
     }
 
+    /// TODO: DOCUMENT
     pub fn env_clear(mut self, value: bool) -> Self {
         self.env_clear = value;
         self
     }
 
+    /// TODO: DOCUMENT
     pub fn current_dir(mut self, value: PathBuf) -> Self {
         self.current_dir = Some(value);
         self
     }
 
+    /// TODO: DOCUMENT
     pub fn entry_point(mut self, value: &str) -> Self {
         self.entry_point = Some(value.to_owned());
         self
     }
 }
 
+/// TODO: DOCUMENT
 #[derive(Debug)]
 pub struct OptionsParser {
+    /// TODO: DOCUMENT
     pub options: Options,
 }
 
 impl OptionsParser {
+    /// TODO: DOCUMENT
     pub fn new(options: Options) -> Self {
         Self { options }
     }
 
+    /// TODO: DOCUMENT
     pub fn into_arg(self) -> OsString {
         let mut arg = OsString::new();
         if !self.options.env_clear {
@@ -88,6 +127,9 @@ impl OptionsParser {
         arg
     }
 
+    /// TODO: DOCUMENT
+    ///
+    /// # Panics
     pub fn from_arg(self, arg: &str) -> Option<Options> {
         let mut options = Options::new();
         for opt in arg.strip_prefix('\'')?.strip_suffix('\'')?.split("','") {
