1dec7e490ea02fa4a01086bf001630c874fbfd0b
gamma0987 <gamma0987@posteo.de>
2023-12-12 20:34:14 +0100

    feat(client-reqs): Improve features structure

--- client-request-tests/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    if cfg!(feature = "client_requests") {
-        println!("cargo:rerun-if-changed=valgrind/wrapper.c");
-        cc::Build::new()
-            .file("valgrind/wrapper.c")
-            .compile("wrapper");
-    }
-}
