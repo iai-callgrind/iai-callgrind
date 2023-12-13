167ddfa7c8c7804404a4c8dd3fdbb3c8854fb5ba
gamma0987 <gamma0987@posteo.de>
2023-12-13 04:41:33 +0100

    feat(client-reqs): Add first tests for callgrind client requests

---
+++ client-request-tests/src/bin/callgrind-reqs-test.rs
@@ -0,0 +1,58 @@
+use client_request_tests::MARKER;
+use iai_callgrind::client_requests;
+
+fn do_work(start: i32) -> i32 {
+    let mut sum = start;
+
+    for i in 1..10 {
+        sum += i;
+    }
+    sum
+}
+
+fn client_requests_1() -> i32 {
+    let mut sum = do_work(0);
+
+    client_requests::callgrind::zero_stats();
+
+    sum += do_work(sum);
+    client_requests::callgrind::dump_stats();
+
+    sum += do_work(sum);
+    client_requests::callgrind::dump_stats_at("Please dump here");
+
+    do_work(sum)
+}
+
+fn client_requests_2() -> i32 {
+    let mut sum = client_requests_1();
+
+    client_requests::callgrind::toggle_collect();
+
+    sum += client_requests_1();
+    client_requests::callgrind::toggle_collect();
+
+    sum
+}
+
+fn main() {
+    iai_callgrind::valgrind_printf!("{MARKER}\n");
+
+    client_requests_2();
+
+    client_requests::callgrind::stop_instrumentation();
+
+    client_requests_2();
+
+    client_requests::callgrind::start_instrumentation();
+
+    client_requests_2();
+
+    client_requests::callgrind::stop_instrumentation();
+
+    client_requests_2();
+
+    client_requests::callgrind::start_instrumentation();
+
+    std::process::exit(client_requests::valgrind::running_on_valgrind() as i32);
+}
--- client-request-tests/src/lib.rs
+++ client-request-tests/src/lib.rs
@@ -1,9 +1,1 @@
-// pub mod bindings {
-//     extern "C" {
-//         pub fn running_on_valgrind() -> cty::size_t;
-//     }
-// }
-
-pub fn running_on_valgrind() -> usize {
-    iai_callgrind::client_requests::valgrind::running_on_valgrind()
-}
+pub const MARKER: &str = "___START___";
