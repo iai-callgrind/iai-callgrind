b6c77f61fae8276184fb2a448f55355af2e3460b
gamma0987 <gamma0987@posteo.de>
2024-01-26 08:09:41 +0100

    tests(benchmark_tests): Add basic support to validate benchmark stdout, stderr

--- benchmark-tests/src/bench.rs
+++ benchmark-tests/src/bench.rs
@@ -1,13 +1,17 @@
 use std::collections::{HashMap, HashSet};
+use std::fmt::Write;
 use std::fs::File;
-use std::io::Read;
+use std::io::{stderr, stdout, BufRead, Read, Write as IOWrite};
 use std::path::{Path, PathBuf};
+use std::process::{Output, Stdio};
 
 use colored::Colorize;
 use glob::glob;
 use iai_callgrind_runner::runner::summary::BenchmarkSummary;
+use lazy_static::lazy_static;
 use minijinja::Environment;
 use once_cell::sync::OnceCell;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use valico::json_schema;
 use valico::json_schema::schema::ScopedSchema;
@@ -16,6 +20,18 @@ const PACKAGE: &str = "benchmark-tests";
 const TEMPLATE_BENCH_NAME: &str = "test_bench";
 static TEMPLATE_DATA: OnceCell<HashMap<String, minijinja::Value>> = OnceCell::new();
 
+lazy_static! {
+    static ref NUMBERS_RE: Regex = Regex::new(
+        r"(?x)
+            (?<desc>\s+.+:\s*)(?<comp1>[0-9]+|N/A)\|(?<comp2>[0-9]+|N/A)
+            (?<diff>
+                (?<diff_percent>(?<white1>\s*)(?<percent>\(.*\)))
+                (?<diff_factor>(?<white2>\s*)(?<factor>\[.*\]))?
+            )?"
+    )
+    .expect("Regex should compile");
+}
+
 #[derive(Debug, Clone)]
 struct Benchmark {
     name: String,
@@ -23,6 +39,9 @@ struct Benchmark {
     config: Config,
     dest_dir: PathBuf,
 }
+
+#[derive(Debug)]
+struct BenchmarkOutput(Output);
 
 #[derive(Debug)]
 pub struct BenchmarkRunner {
@@ -53,6 +72,10 @@ struct Expected {
 struct ExpectedConfig {
     #[serde(default)]
     files: Option<PathBuf>,
+    #[serde(default)]
+    stdout: Option<PathBuf>,
+    #[serde(default)]
+    stderr: Option<PathBuf>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -91,51 +114,6 @@ struct RunConfig {
     expected: Option<ExpectedConfig>,
 }
 
-impl Metadata {
-    pub fn new(benches: &[String]) -> Self {
-        let meta = cargo_metadata::MetadataCommand::new()
-            .no_deps()
-            .exec()
-            .unwrap();
-
-        let package_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        let benches_dir = package_dir.join("benches");
-        let workspace_root = meta.workspace_root.clone().into_std_path_buf();
-        let target_directory = meta.target_directory.clone().into_std_path_buf();
-        let benchmarks = glob(&format!("{}/*.conf.yml", benches_dir.display()))
-            .unwrap()
-            .map(Result::unwrap)
-            .filter(|path| {
-                if benches.is_empty() {
-                    true
-                } else {
-                    let name = path.file_name().unwrap().to_string_lossy();
-                    benches.contains(&name.strip_suffix(".conf.yml").unwrap().to_string())
-                }
-            })
-            .map(|path| Benchmark::new(&path, &package_dir, &target_directory))
-            .collect::<Vec<Benchmark>>();
-
-        Self {
-            workspace_root,
-            target_directory,
-            benchmarks,
-            benches_dir,
-        }
-    }
-
-    pub fn get_bench_file(&self, bench_name: &str) -> PathBuf {
-        self.get_file(format!("{bench_name}.rs"))
-    }
-
-    pub fn get_file<T>(&self, file_name: T) -> PathBuf
-    where
-        T: AsRef<Path>,
-    {
-        self.benches_dir.join(file_name.as_ref())
-    }
-}
-
 impl Benchmark {
     pub fn new(path: &Path, _package_dir: &Path, target_dir: &Path) -> Self {
         let config: Config = serde_yaml::from_reader(File::open(path).expect("File should exist"))
@@ -164,18 +142,30 @@ impl Benchmark {
         }
     }
 
-    pub fn run_bench(&self, args: &[String]) {
+    pub fn run_bench(&self, args: &[String], capture: bool) -> Option<BenchmarkOutput> {
         let mut command = std::process::Command::new(env!("CARGO"));
         command.args(["bench", "--package", PACKAGE, "--bench", &self.bench_name]);
         if !args.is_empty() {
             command.arg("--");
             command.args(args);
         }
-        let status = command
-            .status()
+        let output = command
+            .stderr(if capture {
+                Stdio::piped()
+            } else {
+                Stdio::inherit()
+            })
+            .stdout(if capture {
+                Stdio::piped()
+            } else {
+                Stdio::inherit()
+            })
+            .output()
             .expect("Launching benchmark should succeed");
 
-        assert!(status.success(), "Expected run to be successful");
+        assert!(output.status.success(), "Expected run to be successful");
+
+        capture.then_some(BenchmarkOutput(output))
     }
 
     pub fn run_template(
@@ -184,7 +174,8 @@ impl Benchmark {
         args: &[String],
         template_data: &HashMap<String, minijinja::Value>,
         meta: &Metadata,
-    ) {
+        capture: bool,
+    ) -> Option<BenchmarkOutput> {
         let mut template_string = String::new();
         File::open(meta.get_file(template_path))
             .expect("File should exist")
@@ -199,7 +190,7 @@ impl Benchmark {
         let dest = File::create(meta.get_bench_file(&self.bench_name)).unwrap();
         template.render_to_write(template_data, dest).unwrap();
 
-        self.run_bench(args);
+        self.run_bench(args, capture)
     }
 
     pub fn run(&self, group: &GroupConfig, meta: &Metadata, schema: &ScopedSchema<'_>) {
@@ -207,22 +198,6 @@ impl Benchmark {
 
         let num_runs = group.runs.len();
         for (index, run) in group.runs.iter().enumerate() {
-            let expected_runs = run.expected.as_ref().and_then(|expected| {
-                expected.files.as_ref().map(|expected_files| {
-                    let expected_runs: ExpectedRuns = serde_yaml::from_reader(
-                        File::open(meta.get_file(expected_files)).expect("File should exist"),
-                    )
-                    .map_err(|error| {
-                        format!(
-                            "Failed to deserialize '{}': {error}",
-                            expected_files.display()
-                        )
-                    })
-                    .expect("File should be deserializable");
-                    expected_runs
-                })
-            });
-
             print_info(format!(
                 "Running {}: ({}/{})",
                 &self.name,
@@ -234,18 +209,126 @@ impl Benchmark {
                 print_info(format!("Benchmark arguments: {}", run.args.join(" ")))
             }
 
-            if let Some(template) = &self.config.template {
-                self.run_template(template, &run.args, &run.template_data, meta);
-            } else {
-                self.run_bench(&run.args);
-            }
+            let capture = run
+                .expected
+                .as_ref()
+                .map_or(false, |e| e.stdout.is_some() || e.stderr.is_some());
 
-            if let Some(expected_runs) = expected_runs {
-                for expected_run in expected_runs.data {
-                    expected_run.assert(&self.dest_dir, schema);
-                }
+            let output = if let Some(template) = &self.config.template {
+                self.run_template(template, &run.args, &run.template_data, meta, capture)
+            } else {
+                self.run_bench(&run.args, capture)
+            };
+
+            run.assert(meta, output, schema, &self.dest_dir);
+        }
+    }
+}
+
+impl BenchmarkOutput {
+    fn assert(&self, meta: &Metadata, expected: &ExpectedConfig) {
+        let output = &self.0;
+
+        eprintln!("STDERR:");
+        stderr().write_all(&output.stderr).unwrap();
+        println!("STDOUT:");
+        stdout().write_all(&output.stdout).unwrap();
+
+        if let Some(stderr) = &expected.stderr {
+            let mut expected_stderr: Vec<u8> = Vec::new();
+            File::open(meta.get_file(stderr))
+                .expect("File should exist")
+                .read_to_end(&mut expected_stderr)
+                .expect("Reading file should succeed");
+            let actual = String::from_utf8_lossy(&output.stderr);
+            let expected_string = String::from_utf8_lossy(&expected_stderr);
+            if actual != expected_string {
+                panic!(
+                    "Assertion failed: {}",
+                    pretty_assertions::StrComparison::new(&actual, &expected_string)
+                );
             }
         }
+
+        if let Some(stdout) = &expected.stdout {
+            let mut expected_stdout: Vec<u8> = Vec::new();
+            File::open(meta.get_file(stdout))
+                .expect("File should exist")
+                .read_to_end(&mut expected_stdout)
+                .expect("Reading file should succeed");
+            let filtered = self.filter_stdout(&output.stdout);
+            let expected_string = String::from_utf8_lossy(&expected_stdout);
+            if filtered != expected_string {
+                panic!(
+                    "Assertion failed: {}",
+                    pretty_assertions::StrComparison::new(&filtered, &expected_string)
+                );
+            }
+        }
+    }
+
+    fn filter_stdout(&self, stdout: &[u8]) -> String {
+        let mut result = String::new();
+        for line in stdout.lines().map(Result::unwrap) {
+            if let Some(caps) = NUMBERS_RE.captures(&line) {
+                let mut string = String::new();
+                let desc = caps.name("desc").unwrap().as_str();
+                let comp1 = {
+                    let cap = caps.name("comp1").unwrap().as_str();
+                    if cap.parse::<f64>().is_ok() {
+                        " ".repeat(cap.len())
+                    } else {
+                        cap.to_owned()
+                    }
+                };
+                let comp2 = {
+                    let cap = caps.name("comp2").unwrap().as_str();
+                    if cap.parse::<f64>().is_ok() {
+                        " ".repeat(cap.len())
+                    } else {
+                        cap.to_owned()
+                    }
+                };
+                write!(string, "{desc}{comp1}|{comp2}").unwrap();
+                if caps.name("diff_percent").is_some() {
+                    let white1 = caps.name("white1").unwrap().as_str();
+                    let percent = caps.name("percent").unwrap().as_str();
+                    let num = &percent[1..percent.len() - 2];
+                    if num.parse::<f64>().is_ok() {
+                        write!(
+                            string,
+                            "{white1}({}{}%)",
+                            num.chars().next().unwrap(),
+                            " ".repeat(num.len() - 1)
+                        )
+                        .unwrap();
+                    } else {
+                        write!(string, "{white1}{percent}").unwrap();
+                    }
+                }
+                if caps.name("diff_factor").is_some() {
+                    let white2 = caps.name("white2").unwrap().as_str();
+                    let factor = caps.name("factor").unwrap().as_str();
+                    let num = &factor[1..factor.len() - 2];
+                    if num.parse::<f64>().is_ok() {
+                        write!(
+                            string,
+                            "{white2}[{}{}x]",
+                            num.chars().next().unwrap(),
+                            " ".repeat(num.len() - 1)
+                        )
+                        .unwrap();
+                    } else {
+                        write!(string, "{white2}{factor}").unwrap();
+                    }
+                }
+                writeln!(result, "{string}").unwrap();
+            } else {
+                writeln!(result, "{line}").unwrap();
+            }
+        }
+
+        result
     }
 }
 
@@ -374,6 +457,78 @@ impl ExpectedRun {
             dir.display(),
             real_files
         );
+    }
+}
+
+impl Metadata {
+    pub fn new(benches: &[String]) -> Self {
+        let meta = cargo_metadata::MetadataCommand::new()
+            .no_deps()
+            .exec()
+            .unwrap();
+
+        let package_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let benches_dir = package_dir.join("benches");
+        let workspace_root = meta.workspace_root.clone().into_std_path_buf();
+        let target_directory = meta.target_directory.clone().into_std_path_buf();
+        let benchmarks = glob(&format!("{}/*.conf.yml", benches_dir.display()))
+            .unwrap()
+            .map(Result::unwrap)
+            .filter(|path| {
+                if benches.is_empty() {
+                    true
+                } else {
+                    let name = path.file_name().unwrap().to_string_lossy();
+                    benches.contains(&name.strip_suffix(".conf.yml").unwrap().to_string())
+                }
+            })
+            .map(|path| Benchmark::new(&path, &package_dir, &target_directory))
+            .collect::<Vec<Benchmark>>();
+
+        Self {
+            workspace_root,
+            target_directory,
+            benchmarks,
+            benches_dir,
+        }
+    }
+
+    pub fn get_bench_file(&self, bench_name: &str) -> PathBuf {
+        self.get_file(format!("{bench_name}.rs"))
+    }
+
+    pub fn get_file<T>(&self, file_name: T) -> PathBuf
+    where
+        T: AsRef<Path>,
+    {
+        self.benches_dir.join(file_name.as_ref())
+    }
+}
+
+impl RunConfig {
+    fn assert(
+        &self,
+        meta: &Metadata,
+        output: Option<BenchmarkOutput>,
+        schema: &ScopedSchema<'_>,
+        dest_dir: &Path,
+    ) {
+        if let Some(expected) = &self.expected {
+            if let Some(output) = output {
+                output.assert(meta, expected);
+            }
+
+            if let Some(files) = &expected.files {
+                let expected_runs: ExpectedRuns = serde_yaml::from_reader(
+                    File::open(meta.get_file(files)).expect("File should exist"),
+                )
+                .map_err(|error| format!("Failed to deserialize '{}': {error}", files.display()))
+                .expect("File should be deserializable");
+                for expected in expected_runs.data {
+                    expected.assert(dest_dir, schema);
+                }
+            }
+        }
     }
 }
 
