b6a2e4e159e62f6ab5cd26003540af6aff1cab3e
gamma0987 <gamma0987@posteo.de>
2024-07-25 05:52:08 +0200

    feat(bin_bench): Cleanup benchmark tests and test binaries for binary benchmarks

--- benchmark-tests/src/benchmark-tests-exit.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let status = std::env::args().nth(1).expect("Exit status");
-    let code = status.parse::<i32>().expect("Valid status code");
-    std::process::exit(code);
-}
--- benchmark-tests/src/benchmark-tests-subprocess.rs
+++
@@ -1,7 +0,0 @@
-use benchmark_tests::subprocess;
-
-fn main() {
-    let mut iter = std::env::args().skip(1);
-    let exe = iter.next().unwrap();
-    subprocess(exe, iter).unwrap();
-}
