cc69a9e7469fd838083f85aeca3054790d6414a9
gamma0987 <gamma0987@posteo.de>
2024-07-30 14:46:17 +0200

    tests(file_parameter): Refine benchmark tests

--- benchmark-tests/benches/test_lib_bench_file_parameter.rs
+++ benchmark-tests/benches/test_lib_bench_file_parameter.rs
@@ -1,12 +1,31 @@
 use std::fs::File;
+use std::io::{Read, Write};
 use std::path::PathBuf;
-use std::str::FromStr;
 
-/// See issue https://github.com/iai-callgrind/iai-callgrind/issues/198
-/// Generic bench arguments cause compilation failure
-///
-/// After the fix the benchmark should now compile
 use iai_callgrind::{library_benchmark, library_benchmark_group, main};
+
+fn consume_line(path: &PathBuf) -> String {
+    let mut file = File::open(path).expect("Opening the file for reading should succeed");
+
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer)
+        .expect("Reading the content of the file to a string should succeed");
+    std::fs::remove_file(path).expect("Deleting the file should succeed");
+    if let Some((line, remainder)) = buffer.split_once("\n") {
+        if !remainder.is_empty() {
+            let mut file = File::create(path).expect("(Re-)creating the file should succeed");
+            file.write_all(remainder.as_bytes())
+                .expect("Writing to the file should succeed");
+        }
+        line.trim_end().to_owned()
+    } else {
+        buffer
+    }
+}
+
+fn string_to_u64(line: String) -> u64 {
+    line.parse().unwrap()
+}
 
 #[library_benchmark]
 #[benches::one_line(file = "benchmark-tests/benches/fixtures/one_line.fix")]
@@ -15,23 +34,44 @@ fn one_line(value: String) {
 }
 
 #[library_benchmark]
+#[benches::one_line(file = "benchmark-tests/benches/fixtures/one_line.fix", setup = string_to_u64)]
+fn one_line_with_setup(value: u64) {
+    assert_eq!(value, 1);
+}
+
+#[library_benchmark]
 #[benches::two_lines(file = "benchmark-tests/benches/fixtures/two_lines.fix")]
 fn two_lines(value: String) {
-    let lock_file = "/tmp/iai-callgrind.two_lines.lock";
-    if &value == "11" {
-        if PathBuf::from_str(lock_file).unwrap().exists() {
-            panic!("The lock file '{lock_file}' should not exist");
-        }
-        File::create(lock_file).unwrap();
-    } else {
-        std::fs::remove_file(lock_file).unwrap();
-        assert_eq!(value, "111".to_owned());
+    let path = PathBuf::from("/tmp/iai-callgrind.two_lines.fix");
+    if !path.exists() {
+        std::fs::create_dir_all(path.parent().unwrap())
+            .expect("The temporary directory should exist or being creatable");
+        std::fs::copy("benches/fixtures/two_lines.fix", &path)
+            .expect("Copying the fixture should succeed");
     }
+
+    let expected = consume_line(&path);
+    assert_eq!(value, expected);
+}
+
+#[library_benchmark]
+#[benches::two_lines(file = "benchmark-tests/benches/fixtures/two_lines.fix", setup = string_to_u64)]
+fn two_lines_with_setup(value: u64) {
+    let path = PathBuf::from("/tmp/iai-callgrind.two_lines.fix");
+    if !path.exists() {
+        std::fs::create_dir_all(path.parent().unwrap())
+            .expect("The temporary directory should exist or being creatable");
+        std::fs::copy("benches/fixtures/two_lines.fix", &path)
+            .expect("Copying the fixture should succeed");
+    }
+
+    let expected = consume_line(&path);
+    assert_eq!(value, expected.parse::<u64>().unwrap());
 }
 
 library_benchmark_group!(
     name = bench_group;
-    benchmarks = one_line, two_lines
+    benchmarks = one_line, one_line_with_setup, two_lines, two_lines_with_setup
 );
 
 main!(library_benchmark_groups = bench_group);
