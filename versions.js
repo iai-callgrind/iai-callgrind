9682146e77daf9c1994788d462cc55f54888047d
gamma0987 <gamma0987@users.noreply.github.com>
2024-08-28 16:05:47 +0000

    deploy: 52fb160a8418f78771b8027bc129e353418e7dbf

--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 document.write(
   '\
 <!-- Insert new version here -->\
+<a href="/iai-callgrind/0.13.1/html/index.html">0.13.1</a>\
 <a href="/iai-callgrind/0.13.0/html/index.html">0.13.0</a>\
 <a href="/iai-callgrind/0.12.3/index.html">0.12.3</a>\
 ',
