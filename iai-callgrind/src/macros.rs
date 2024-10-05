ac024e1d703c13a3e81ce7fe9953f36909453f40
gamma0987 <gamma0987@posteo.de>
2024-10-05 11:57:19 +0200

    Bump version v0.13.4 -> v0.14.0

--- iai-callgrind/src/macros.rs
+++ iai-callgrind/src/macros.rs
@@ -235,7 +235,7 @@ macro_rules! main {
             let exe = option_env!("IAI_CALLGRIND_RUNNER")
                 .unwrap_or_else(|| option_env!("CARGO_BIN_EXE_iai-callgrind-runner").unwrap_or("iai-callgrind-runner"));
 
-            let library_version = "0.13.4";
+            let library_version = "0.14.0";
 
             let mut cmd = std::process::Command::new(exe);
 
@@ -594,7 +594,7 @@ macro_rules! main {
             let exe = option_env!("IAI_CALLGRIND_RUNNER")
                 .unwrap_or_else(|| option_env!("CARGO_BIN_EXE_iai-callgrind-runner").unwrap_or("iai-callgrind-runner"));
 
-            let library_version = "0.13.4";
+            let library_version = "0.14.0";
 
             let mut cmd = std::process::Command::new(exe);
 
