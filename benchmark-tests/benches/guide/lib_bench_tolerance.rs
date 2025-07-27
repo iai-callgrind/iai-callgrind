af7944bc748864ce51c569a45f991374989262b0
gamma0987 <gamma0987@posteo.de>
2025-07-27 09:28:15 +0200

    docs(lib+guide): Update documentation for --tolerance, ...

---
+++ benchmark-tests/benches/guide/lib_bench_tolerance.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+use std::hint::black_box;
+
+use iai_callgrind::{
+    library_benchmark, library_benchmark_group, main, LibraryBenchmarkConfig, OutputFormat,
+};
+
+fn make_hashmap(num: usize) -> HashMap<String, usize> {
+    (0..num).fold(HashMap::new(), |mut acc, e| {
+        acc.insert(format!("element: {e}"), e);
+        acc
+    })
+}
+
+#[library_benchmark(
+    config = LibraryBenchmarkConfig::default()
+        .output_format(OutputFormat::default()
+            .tolerance(0.9)
+        )
+)]
+#[bench::tolerance(make_hashmap(100))]
+fn bench_hash_map(map: HashMap<String, usize>) -> Option<usize> {
+    black_box(
+        map.iter()
+            .find_map(|(key, value)| (key == "element: 12345").then_some(*value)),
+    )
+}
+
+library_benchmark_group!(name = my_group; benchmarks = bench_hash_map);
+main!(library_benchmark_groups = my_group);
